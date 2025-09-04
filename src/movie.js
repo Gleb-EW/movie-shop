40c80d86becac3e2154560dd991a434b92b075a3
Gleb <rakitin-gleb@mail.ru>
2025-09-04 10:33:16 +0300

    Add Movie and Cart implementations with tests

--- src/movie.js
+++ src/movie.js
@@ -1,21 +1,44 @@
 class Movie {
-  constructor({ title, year, country, slogan, genre, duration }) {
+  /**
+   * @param {Object} options
+   * @param {number} options.id
+   * @param {string} options.title
+   * @param {string} options.titleEng
+   * @param {string} options.format
+   * @param {number} options.year
+   * @param {string} options.country
+   * @param {string} options.slogan
+   * @param {string[]} options.genre
+   * @param {string} options.duration
+   * @param {number} options.price
+   */
+  constructor({ id, title, titleEng, format, year, country, slogan, genre, duration, price }) {
+    this.id = id;
     this.title = title;
+    this.titleEng = titleEng;
+    this.format = format;
     this.year = year;
-    this.country = country || 'Неизвестно';
-    this.slogan = slogan || '';
-    this.genre = genre || [];
-    this.duration = duration || '';
+    this.country = country;
+    this.slogan = slogan;
+    this.genre = genre;
+    this.duration = duration;
+    this.price = price;
   }
 
+  /**
+   * Возвращает информацию о фильме
+   * @returns {string}
+   */
   getInfo() {
     return `
-${this.title} (${this.year})
-Страна: ${this.country}
-Слоган: ${this.slogan}
-Жанр: ${this.genre.join(', ')}
-Время: ${this.duration}
-`;
+    ${this.title} (${this.titleEng}), ${this.format}
+    Год: ${this.year}
+    Страна: ${this.country}
+    Слоган: "${this.slogan}"
+    Жанр: ${this.genre.join(', ')}
+    Время: ${this.duration}
+    Цена: ${this.price}₽
+    `;
   }
 }
 
--- tests/movie.test.js
+++ tests/movie.test.js
@@ -1,18 +1,48 @@
 const Movie = require('../src/movie.js');
 
 describe('Movie', () => {
-  test('должен корректно создавать объект фильма', () => {
+  test('создаёт объект фильма с правильными полями', () => {
     const avengers = new Movie({
+      id: 1,
       title: 'Мстители',
+      titleEng: 'The Avengers',
+      format: 'IMAX',
       year: 2012,
       country: 'США',
       slogan: 'Avengers Assemble!',
       genre: ['фантастика', 'боевик', 'фэнтези', 'приключения'],
-      duration: '137 мин. / 02:17'
+      duration: '137 мин. / 02:17',
+      price: 350
     });
 
     expect(avengers.title).toBe('Мстители');
+    expect(avengers.titleEng).toBe('The Avengers');
+    expect(avengers.format).toBe('IMAX');
+    expect(avengers.year).toBe(2012);
     expect(avengers.genre).toContain('боевик');
+    expect(avengers.price).toBe(350);
     expect(avengers.getInfo()).toMatch(/Мстители/);
+  });
+
+  test('фильм можно добавить в корзину', () => {
+    const cart = [];
+    const movie = new Movie({
+      id: 2,
+      title: 'Железный человек',
+      titleEng: 'Iron Man',
+      format: 'IMAX',
+      year: 2008,
+      country: 'США',
+      slogan: 'Heroes aren’t born. They’re built.',
+      genre: ['фантастика', 'боевик'],
+      duration: '126 мин. / 02:06',
+      price: 299
+    });
+
+    cart.push(movie);
+
+    expect(cart.length).toBe(1);
+    expect(cart[0].title).toBe('Железный человек');
+    expect(cart[0].price).toBe(299);
   });
 });
